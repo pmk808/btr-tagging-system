29a1a98673850049aa103df0013371b652d71ae6
BRYLLE KENNETH CANILANZA <kencanilanza@gmail.com>
2024-03-04 16:50:46 +0800

    gikan kang mcphy

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './routes.js';
 import '@fortawesome/fontawesome-free/css/all.css';
+import store from './store';
 
 const app = createApp(App);
 app.use(router); 
